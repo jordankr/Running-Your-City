5be08830311b3e9eb7daf9d8384ca75e2752efd8
Rob <acksailor@gmail.com>
2017-05-16 12:17:12 -0700

    First pass
    Data imported, no gender yet

--- preprocessing.py
+++ preprocessing.py
@@ -6,3 +6,14 @@ Created on Tue May 16 10:59:38 2017
 @author: rob
 """
 
+# Importing libraries
+import numpy as np
+import pandas as pd
+from gender_detector.gender_detector import GenderDetector
+
+# Importing data    
+dataset = pd.read_json("https://data.cityofnewyork.us/resource/4qxi-jgbe.json?$$app_token=2k2aLRGeC9aJ4prhwgaQv0Rar")
+
+# Processing data
+detector = GenderDetector('us')
+new = dataset.assign(gender = lambda x: detector.guess(x['first_name']))
